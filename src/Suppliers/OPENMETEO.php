6bacb289b6d64e38e5b2bdd16015adaeb1414bf5
rafacunha033 <rafacunha.prof033@gmail.com>
2023-07-31 07:27:58 -0300

    Fetch forecast information on OPENMETEO

--- src/Suppliers/OPENMETEO.php
+++ src/Suppliers/OPENMETEO.php
@@ -7,19 +7,40 @@ use PhpWeather\Interfaces\WeatherSupplierInterface;
 
 class OPENMETEO implements WeatherSupplierInterface
 {
+    public $foreacast_api_url = "https://api.open-meteo.com/v1/forecast?";
     public $geocoding_api_url = "https://geocoding-api.open-meteo.com/v1/search?";
 
     public function __construct()
     {
         
     }
-    
-    public function fetchWeatherInformation()
+    // latitude=52.52&longitude=13.41&hourly=temperature_2m
+    public function fetchWeatherInformation($latitude, $longitude)
     {
-        echo 'hi';
+        $ch = curl_init();
+       
+        $params = [
+            "latitude" => $latitude,
+            "longitude" => $longitude,
+            "timezone" => "America/Sao_Paulo",
+            "daily" => ["temperature_2m_max", "temperature_2m_min"]
+        ];
+
+        $url = $this->foreacast_api_url.http_build_query($params);
+    
+        $options = [
+            CURLOPT_URL => $url,
+            CURLOPT_RETURNTRANSFER => true
+        ];
+        
+        curl_setopt_array($ch, $options);
+        
+        $response = curl_exec($ch);
+
+        return $response;   
     }
 
-    public function fetchCoordinates(string $city)
+    public function fetchCoordinates(string $city): string
     {
         $ch = curl_init();
        
@@ -40,7 +61,6 @@ class OPENMETEO implements WeatherSupplierInterface
         $response = curl_exec($ch);
 
         return $response;     
-       
     }
 
 
