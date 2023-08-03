ae672a24d56ce1d5277efade893c0014c115437a
rafacunha033 <rafacunha.prof033@gmail.com>
2023-08-03 11:00:12 -0300

    WeatherSupplierInterface must return the forecast. params: ,

--- src/Interfaces/WeatherSupplierInterface.php
+++ src/Interfaces/WeatherSupplierInterface.php
@@ -3,6 +3,5 @@
 namespace PhpWeather\Interfaces;
 
 interface WeatherSupplierInterface {
-    public function fetchCurrentWeather(string $city);
-    public function fetchCoordinates(string $city);
+    public function fetchForecast($latitude, $longitude);
 }
