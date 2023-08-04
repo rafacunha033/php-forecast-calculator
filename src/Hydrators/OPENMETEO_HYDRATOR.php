4af4597f34fa6acc72f2933c2ac027919d967fe1
rafacunha033 <rafacunha.prof033@gmail.com>
2023-08-04 19:48:34 -0300

    values changed

--- src/Hydrators/OPENMETEO_HYDRATOR.php
+++ src/Hydrators/OPENMETEO_HYDRATOR.php
@@ -14,8 +14,8 @@ class OPENMETEO_HYDRATOR implements SupplierHydratorInterface
         $responseArray = [];
         for($i = 0; $i < $daysCount ; $i++) {
             $responseArray[$dailyForecast["time"][$i]] = [
-                "temperature_2m_max" => $dailyForecast["temperature_2m_max"][$i],
-                "temperature_2m_min" => $dailyForecast["temperature_2m_min"][$i]
+                "temperature_max" => $dailyForecast["temperature_2m_max"][$i],
+                "temperature_min" => $dailyForecast["temperature_2m_min"][$i]
             ];
         }
 
