0a6ec92cd8d349cd6d82b2d525fa3b33954c6fbf
rafacunha033 <rafacunha.prof033@gmail.com>
2023-08-04 11:27:14 -0300

    /Geocoding/OPENMETEO_GEOCODING created

---
+++ src/Geocoding/OPENMETEO_GEOCODING.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace PhpWeather\Geocoding;
+
+use PhpWeather\Interfaces\GeocodingInterface;
+
+class OPENMETEO_GEOCODING implements GeocodingInterface
+{
+
+    public $geocoding_api_url = "https://geocoding-api.open-meteo.com/v1/search?";
+    
+    public function fetchCoordinatesAndTimezone(string $city)
+    {
+        $ch = curl_init();
+       
+        $params = [
+            "name" => $city,
+            "format" => "json"
+        ];
+
+        $url = $this->geocoding_api_url.http_build_query($params);
+    
+        $options = [
+            CURLOPT_URL => $url,
+            CURLOPT_RETURNTRANSFER => true
+        ];
+        
+        curl_setopt_array($ch, $options);
+        
+        $response = json_decode(curl_exec($ch), true);
+
+        $firstResult = $response["results"][0];
+        
+        return [
+            $firstResult["latitude"],
+            $firstResult["longitude"],
+            $firstResult["timezone"]
+        ];     
+    }
+}
